073a1d76b53f25220e96b3021cdeaeb5c1d361ea
Jerome Vercide <jeromevercide@Jeromes-MBP.lan>
2022-01-12 14:35:39 -0500

    update: bump package version to test lerna detection

--- packages/Button/Button.tsx
+++ packages/Button/Button.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
 interface ButtonProps {
-
+        text:string
 }
 
-export const Button: React.FC<ButtonProps> = ({ }) => {
+export const Button: React.FC<ButtonProps> = ({text:string}) => {
         return ();
 }
 
